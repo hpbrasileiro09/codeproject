5e630ab23d3205741c20115899ab6216745867d1
hpbrasileiro <hernando@hpbtec.com.br>
2016-04-11 20:43:36 -0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+
+class ClientController extends Controller
+{
+    
+    public function index() 
+    {
+    	return \CodeProject\Client::all();
+    }
+    
+    public function store(Request $request)
+    {
+    	return \CodeProject\Client::create($request->all());
+    }
+    
+    public function show($id)
+    {
+    	return \CodeProject\Client::find($id);
+    }
+    
+    public function destroy($id)
+    {
+		try
+	    {
+	    	\CodeProject\Client::find($id)->delete();
+	        return response()->json(['OK']);
+	    }
+	    catch (\Exception $e)
+	    {
+	        return response()->json(['NOK' => $e->getMessage()]);
+	    }    	
+	}
+
+}
