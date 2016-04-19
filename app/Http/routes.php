4a60c0e139334c20a5be47690cbb53d1e0f5bec7
hpbrasileiro <hernando@hpbtec.com.br>
2016-04-19 07:57:17 -0300

    Em Routes comentario no middleware removido

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,15 +19,19 @@ Route::post('oauth/access_token', function () {
 	return Response::json(Authorizer::issueAccessToken());
 });
 
+Route::post('oauth2/token', function () {
+	return Response::json(Authorizer::issueAccessToken());
+});
+
 Route::group(['middleware' => 'oauth'], function() {
 
 	Route::resource('client', 'ClientController', ['except' => ['create','edit']]);
 
-	//Route::group(['middleware' => 'check-project-owner'], function() {
+	Route::group(['middleware' => 'check-project-owner'], function() {
 	
 		Route::resource('project', 'ProjectController', ['except' => ['create','edit']]);
 	
-	//});
+	});
 
 	Route::group(['prefix' => 'project'], function() {
 
